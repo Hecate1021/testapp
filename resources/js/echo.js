6071579d59c8670c276e119e1981fe0f53b65751
Hecate1021 <86115613+Hecate1021@users.noreply.github.com>
2024-10-12 11:14:01 +0800

    Update echo.js

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,11 +1,14 @@
 import Echo from 'laravel-echo';
 
+import Pusher from 'pusher-js';
+window.Pusher = Pusher;
+
 window.Echo = new Echo({
-    broadcaster: 'reverb',  // Use Reverb as the broadcaster
-    key: import.meta.env.VITE_REVERB_APP_KEY,  // Reverb app key
-    wsHost: import.meta.env.VITE_REVERB_HOST,  // Reverb WebSocket host
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,  // WebSocket port (default to 80)
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,  // Secure WebSocket port (default to 443)
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',  // Force TLS if scheme is https
-    enabledTransports: ['ws', 'wss'],  // Enable WebSocket and Secure WebSocket
+    broadcaster: 'reverb',
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    enabledTransports: ['ws', 'wss'],
 });
